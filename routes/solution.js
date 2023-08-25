f0053fc8fe723ef7d52e1c75e8febf2754809b57
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-08-25 22:57:44 +0100

    solution route

--- routes/solution.js
+++ routes/solution.js
@@ -2,12 +2,12 @@ const router = require('express').Router()
 const verify = require('./verifyToken')
 const helpers = require('../helpers/solution')
 
-router.route('/').get(helpers.getAllItems).post(verify, helpers.createItem)
+router.route('/').get(helpers.getAll).post(helpers.create)
 
-router
-  .route('/:itemId')
-  .get(helpers.getOneItem)
-  .put(verify, helpers.updateItem)
-  .delete(verify, helpers.deleteItem)
+// router
+//   .route('/:itemId')
+//   .get(helpers.getOneItem)
+//   .put(verify, helpers.updateItem)
+//   .delete(verify, helpers.deleteItem)
 
 module.exports = router
