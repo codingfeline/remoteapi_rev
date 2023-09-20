187799fac2dfc74c3234a4304ed7bdc00aaf2017
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-09-20 05:14:31 +0100

    solution routes

--- routes/solution.js
+++ routes/solution.js
@@ -3,6 +3,11 @@ const verify = require('./verifyToken')
 const helpers = require('../helpers/solution')
 
 router.route('/').get(helpers.getAll).post(helpers.create)
+router
+  .route('/:id')
+  .get(helpers.getOneItem)
+  .put(helpers.updateMenu)
+  .delete(helpers.deleteMenu)
 
 // router
 //   .route('/:itemId')
