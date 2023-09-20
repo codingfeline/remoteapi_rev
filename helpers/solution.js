d1ed04a88a01fac2fe36a0273260577349be392e
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-09-20 05:14:04 +0100

    get, create, delete

--- helpers/solution.js
+++ helpers/solution.js
@@ -7,7 +7,7 @@ exports.getAll = (req, res) => {
 }
 
 exports.getOneItem = (req, res) => {
-  Solution.findById(req.params.menuId)
+  Solution.findById(req.params.id)
     .then(menu => res.json(menu))
     .catch(err => res.send(err))
 }
@@ -15,20 +15,23 @@ exports.getOneItem = (req, res) => {
 exports.create = async (req, res) => {
   await Solution.create(req.body)
     .then(newItem => {
-      console.log(newItem)
+      console.log('solution > create', newItem)
     })
     .then(newItem => res.status(201).json(newItem))
     .catch(err => res.send(err))
 }
 
 exports.deleteMenu = async (req, res) => {
-  await Solution.findOneAndDelete({ _id: req.params.menuId })
+  await Solution.findOneAndDelete({ _id: req.params.id })
     .then(() => res.json({ message: 'Item deleted' }))
     .catch(err => res.send(err))
 }
 
 exports.updateMenu = async (req, res) => {
-  await Solution.findOneAndUpdate({ _id: req.params.menuId }, req.body, { new: true })
+  // console.log(req.params)
+  // console.log(req.body)
+  // return
+  await Solution.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
     .then(menu => res.json(menu))
     .catch(err => res.send(err))
 }
